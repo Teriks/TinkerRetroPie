969bafcd62ad11b180c30887c5ffdf60534585d7
Teriks <Teriks@users.noreply.github.com>
2018-09-26 13:30:41 -0500

    Remove kernel config prompt from reproduce_cur_release_img.sh

--- reproduce_cur_release_img.sh
+++ reproduce_cur_release_img.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build_installer.sh BUILD_ARMBIAN=yes KERNEL_CONFIGURE=yes KERNELBRANCH=tag:v4.14.71 LIB_TAG=c1530db
+./build_installer.sh BUILD_ARMBIAN=yes KERNEL_CONFIGURE=no KERNELBRANCH=tag:v4.14.71 LIB_TAG=c1530db
