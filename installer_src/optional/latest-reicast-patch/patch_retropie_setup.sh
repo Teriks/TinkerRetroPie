4e4e42623014d034c99f68d2424f82f8bbd438e9
Teriks <Teriks@users.noreply.github.com>
2018-09-26 23:27:12 -0500

    Make latest-reicast-patch more robust

--- installer_src/optional/latest-reicast-patch/patch_retropie_setup.sh
+++ installer_src/optional/latest-reicast-patch/patch_retropie_setup.sh
@@ -25,10 +25,22 @@ done
 OG_SCRIPT="$1/scriptmodules/emulators/reicast.sh"
 
 
-if ! [ -f "$OG_SCRIPT.bak" ]; then
-    cp "$OG_SCRIPT" "$OG_SCRIPT.bak"
-
+if ! [ -f "$OG_SCRIPT.tinkerretropie.bak" ]; then
+    cp "$OG_SCRIPT" "$OG_SCRIPT.tinkerretropie.bak"
+else
+    cp "$OG_SCRIPT.tinkerretropie.bak" "$OG_SCRIPT"
 fi
 
-sed "s|TINKER_RETRO_PIE_OPTIONAL_NEW_REICAST_BRANCH|$BRANCH|
-     s|TINKER_RETRO_PIE_OPTIONAL_NEW_REICAST|$(realpath $SCRIPTPATH)|" "$SCRIPTPATH/reicast.sh.template" > "$OG_SCRIPT"
+pushd "$1" > /dev/null
+if git apply "$SCRIPTPATH/reicast.sh.patch"; then
+    sed -i "s|TINKER_RETRO_PIE_OPTIONAL_NEW_REICAST_BRANCH|$BRANCH|
+            s|TINKER_RETRO_PIE_OPTIONAL_NEW_REICAST|$(realpath $SCRIPTPATH)|" "$OG_SCRIPT"
+
+    echo "=========="
+    echo "Patch Applied."
+else
+    echo "=========="
+    echo "Could not patch $OG_SCRIPT"
+    exit 1
+fi
+popd > /dev/null
