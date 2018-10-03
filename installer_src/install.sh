997d42c785cc38b5c5dab643cad96ddf1431d861
Teriks <Teriks@users.noreply.github.com>
2018-10-02 19:49:08 -0500

    Fix how spaces are handled in install script parameters

--- installer_src/install.sh
+++ installer_src/install.sh
@@ -66,8 +66,13 @@ for i in "$@"; do
     if [[ $i == *=* ]]; then
         parameter=${i%%=*}
         value=${i##*=}
-        echo "Command line: setting $parameter to" "${value:-(empty)}"
-        eval $parameter=$value
+        if [[ "$value" =~ ' ' ]]; then
+            echo "Command line: setting $parameter to" "\"${value:-(empty)}\""
+            eval $parameter="\"$value\""
+        else
+            echo "Command line: setting $parameter to" "${value:-(empty)}"
+            eval $parameter=$value
+        fi
     fi
 done
 
