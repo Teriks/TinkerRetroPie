c8610ebbd44e8b66f1fd6a5cdf0a38608098f76f
Teriks <Teriks@users.noreply.github.com>
2018-10-08 08:44:50 -0500

    Revert release versions

--- tools/reproduce_cur_release.sh
+++ tools/reproduce_cur_release.sh
@@ -7,6 +7,6 @@ SCRIPTPATH="$(
 
 "$SCRIPTPATH/../build_installer.sh" BUILD_ARMBIAN=yes \
                                     KERNEL_CONFIGURE=no \
-                                    KERNELBRANCH=tag:v4.14.74 \
-                                    LIB_TAG=b971c3f \
+                                    KERNELBRANCH=tag:v4.14.71 \
+                                    LIB_TAG=c1530db \
                                     TINKER_RETROPIE_CONFIG="$SCRIPTPATH/cur_installer.cfg"
