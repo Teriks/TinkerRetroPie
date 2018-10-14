8e77e46cba1bea5f3c4ad85337a3189067ae0621
Teriks <Teriks@users.noreply.github.com>
2018-10-14 02:51:33 -0500

    Remove KERNEL_CONFIGURE=yes from build_release.sh, debugging leftover

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -6,7 +6,7 @@ SCRIPTPATH="$(
 )"
 
 "$SCRIPTPATH/../build_installer.sh" BUILD_ARMBIAN=yes \
-                                    KERNEL_CONFIGURE=yes \
+                                    KERNEL_CONFIGURE=no \
                                     KERNELBRANCH=tag:v4.14.76 \
                                     LIB_TAG=6f0b32a \
                                     BUILD_CONTAINER=docker \
