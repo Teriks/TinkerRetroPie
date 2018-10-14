b10cd094faf646aac2aa3d14d7eb7ce26864e11e
Teriks <Teriks@users.noreply.github.com>
2018-10-14 04:33:51 -0500

    Bump Armbian to commit with hci H5 protocol enabled in kconfig

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -8,6 +8,6 @@ SCRIPTPATH="$(
 "$SCRIPTPATH/../build_installer.sh" BUILD_ARMBIAN=yes \
                                     KERNEL_CONFIGURE=no \
                                     KERNELBRANCH=tag:v4.14.76 \
-                                    LIB_TAG=6f0b32a \
+                                    LIB_TAG=1c4340b \
                                     BUILD_CONTAINER=docker \
                                     TINKER_RETROPIE_CONFIG="$SCRIPTPATH/installer.cfg"
