e39f1836cf7c79564a6b637f67a64de48bb939f7
Teriks <Teriks@users.noreply.github.com>
2019-06-09 10:52:04 -0500

    Correct dev release kernel version

--- tools/build_dev_release.sh
+++ tools/build_dev_release.sh
@@ -7,7 +7,7 @@ SCRIPTPATH="$(
 
 "$SCRIPTPATH/../build_installer.sh" BUILD_ARMBIAN=yes \
                                     KERNEL_CONFIGURE=no \
-                                    KERNELBRANCH=tag:v4.19.9 \
+                                    KERNELBRANCH=tag:v5.1.5 \
                                     LIB_TAG=1c3fde7 \
                                     BUILD_CONTAINER=docker \
                                     TINKER_RETROPIE_CONFIG="$SCRIPTPATH/installer.cfg"
